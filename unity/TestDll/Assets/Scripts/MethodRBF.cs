d012d10a19784182db91a597a6d47291d5bf2650
amauryESGI <amaury.colmant@wanadoo.fr>
2016-04-15 16:07:50 +0200

    Add integration regression of RBF

--- unity/TestDll/Assets/Scripts/MethodRBF.cs
+++ unity/TestDll/Assets/Scripts/MethodRBF.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
 
 public class MethodRBF : MonoBehaviour {
     [SerializeField]
     private GameObject _boules;
     [SerializeField]
     private GameObject _examples;
+    [SerializeField]
+    private Material _defaultColor;
 
     private List<Ball>      _balls;
     private List<Ball>      _exBalls;
@@ -44,15 +44,7 @@ public class MethodRBF : MonoBehaviour {
             results.Add(b.c == types[0] ? -1 : 1);//Si la couleur de la boule est la couleur actuelle tu met -1, sinon 1
         }
 
-        var str = "";
-        inputs.ForEach(i => str += ", " + i.ToString());
-        Debug.Log(str);
-        str = "";
-        results.ForEach(i => str += ", " + i.ToString());
-        Debug.Log(str);
-
         WrapperDllMachineLearning.RBF.Fit(model, inputs.ToArray(), 2, inputs.Count, results.ToArray(), _gamma);
-
 
         foreach (var b in _balls) {
             var wanted = new double[] { b.transform.position.x, b.transform.position.z };
@@ -71,9 +63,36 @@ public class MethodRBF : MonoBehaviour {
     }
 
     public void StartRegression() {
+        _exBalls = new List<Ball>();
+
+        var types = new List<EColor>();
+        for (var i = 0; i < _examples.transform.childCount; ++i) {
+            types.Add(_examples.transform.GetChild(i).GetChild(0).GetComponent<Ball>().c);
+            for (var j = 0; j < _examples.transform.GetChild(i).childCount; ++j)
+                _exBalls.Add(_examples.transform.GetChild(i).GetChild(j).GetComponent<Ball>());
+        }
+
+        var model = WrapperDllMachineLearning.RBF.CreateModel(_exBalls.Count, 2);
+
+        var inputs = new List<double>();
+        var results = new List<double>();
+        foreach (var b in _exBalls) {
+            inputs.Add(b.transform.position.x);
+            inputs.Add(b.transform.position.z);
+            results.Add(b.c == types[0] ? -1 : 1);//Si la couleur de la boule est la couleur actuelle tu met -1, sinon 1
+        }
+
+        WrapperDllMachineLearning.RBF.Fit(model, inputs.ToArray(), 2, inputs.Count, results.ToArray(), _gamma);
+
+        foreach (var b in _balls) {
+            var wanted = new double[] { b.transform.position.x, b.transform.position.z };
+
+            var coef = (float)WrapperDllMachineLearning.RBF.Regression(model, wanted, wanted.Length, _gamma);
+            b.renderer.material.color = new Color(Mathf.Max(0, coef * _defaultColor.color.r), 0, Mathf.Max(0, _defaultColor.color.b * -coef));
+        }
     }
 
     void OnDestroy() {
-        WrapperDllMachineLearning.RBF.DestroyModel(_model, 2);
+        WrapperDllMachineLearning.RBF.DestroyModel(_model, _exBalls.Count);
     }
 }
