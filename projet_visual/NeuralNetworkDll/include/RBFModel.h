9346ec8867d97b4b93d9f29048164b23f939bcaf
VisualPi <gintakun2@yahoo.fr>
2016-05-03 12:20:58 +0200

    projet CMAKE

---
+++ projet_visual/NeuralNetworkDll/include/RBFModel.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifdef DLL_EXPORT
+#define DLL_MODE __declspec(dllexport) 
+#else
+#define DLL_MODE __declspec(dllimport) 
+#endif
+
+extern "C"
+{
+    struct RBF {
+        double  W;
+        double* X; // Input value foreach parameter
+        int     nb_parameter;
+        int     nb_samples;
+    };
+
+    DLL_MODE RBF*   RBF_CreateModel(int nb_parameter, int nb_example);
+    DLL_MODE void   RBF_DestroyModel(RBF* model, int nb_example);
+    DLL_MODE double RBF_Classify(RBF * model, double* input, int inputSize, double gamma);
+    DLL_MODE double RBF_Regression(RBF * model, double* input, int inputSize, double gamma);
+    DLL_MODE void   RBF_Fit(RBF * model, double* inputs, int modelSize, int inputsSize, double* results, double gamma);
+    DLL_MODE void   RBF_LLoyd(double * model, double* inputs, int modelSize, int inputsSize, double* results);
+    
+    double NormeEuclidienne(double* x, double* y, int modelSize);
+}
